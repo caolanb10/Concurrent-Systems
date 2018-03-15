b07e8045cd5ae9e55cd9d8660eb4de085b3b1d58
Caolan <caolan.barry1@gmail.com>
2018-03-15 21:45:11 +0000

    started parallel search

---
+++ parallelsearch.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+#include <string.h>
+#include <time.h>
+#include <omp.h>
+
+int linearSearch(int* array, int size, int find)
+{
+  for(int i = 0; i<size; i++)
+  {
+    if(array[i] == find)
+    {
+      printf("%d is found at position: %d. \n",find,i);
+      return 1;
+    }
+  }
+  return 0;
+}
+
+int main()
+{
+  if(argc!=2)
+  {
+    fprintf(stderr, "%s", "error, input quantity parameter");
+    return 1;
+  }
+  clock_t start = clock();
+  char* a = argv[1];
+  int n = atoi(a);
+
+  //Search for an random index within our range of numbers
+  srand(time(NULL));
+  int findIndex = rand() / (RAND_MAX/n+1);
+  FILE* fp;
+  fp = fopen("100000000numbers","r");
+  int* array = malloc(n * sizeof(int));
+
+  for(int index = 0; index<n; index++)
+  {
+    fscanf(fp, "%d", &array[index]);
+  }
+  int number = array[findIndex];
+  linearSearch(array,n,number);
+  clock_t end = clock();
+  double elapsedTime = (double) (end-start)/CLOCKS_PER_SEC;
+  printf("The time elapsed to find a number in an array of size %d is: %f\n",n ,elapsedTime);
+
+
+}
