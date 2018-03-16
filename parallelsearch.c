45be5515a8ebac70aa73df8e993902db52b6878b
Caolan <caolan.barry1@gmail.com>
2018-03-16 14:46:41 +0000

    added parallel

--- parallelsearch.c
+++ parallelsearch.c
@@ -4,31 +4,35 @@
 #include <string.h>
 #include <time.h>
 #include <omp.h>
+#define NUM_THREADS 4
 
 int linearSearch(int* array, int size, int find)
 {
+  int found = -1;
+  #pragma omp parallel for default(none) shared(array, size, find) private(found)
   for(int i = 0; i<size; i++)
   {
     if(array[i] == find)
     {
-      printf("%d is found at position: %d. \n",find,i);
-      return 1;
+      found = i+1;
     }
   }
-  return 0;
+  return found;
 }
 
-int main()
+
+
+int main(int argc, char* argv[])
 {
   if(argc!=2)
   {
     fprintf(stderr, "%s", "error, input quantity parameter");
     return 1;
   }
+
   clock_t start = clock();
   char* a = argv[1];
   int n = atoi(a);
-
   //Search for an random index within our range of numbers
   srand(time(NULL));
   int findIndex = rand() / (RAND_MAX/n+1);
@@ -41,7 +45,19 @@ int main()
     fscanf(fp, "%d", &array[index]);
   }
   int number = array[findIndex];
-  linearSearch(array,n,number);
+  //linearSearch(array,n,number);
+  int size = n;
+  int find = number;
+  int found = -1;
+  #pragma omp parallel for default(none) shared(array, size, find) private(found)
+  for(int i = 0; i<size; i++)
+  {
+    if(array[i] == find)
+    {
+      found = i+1;
+    }
+  }
+
   clock_t end = clock();
   double elapsedTime = (double) (end-start)/CLOCKS_PER_SEC;
   printf("The time elapsed to find a number in an array of size %d is: %f\n",n ,elapsedTime);
